3ed6ec515fc2b1f48c4bee27502639496ce950c2
usmanov <azikusmanov882@gmail.com>
2024-02-19 17:59:30 +0500

    first commit

---
+++ src/main/java/com/example/prestigeportfoliocreators/PrestigePortfolioCreatorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.prestigeportfoliocreators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestigePortfolioCreatorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrestigePortfolioCreatorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prestigeportfoliocreators/PrestigePortfolioCreatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prestigeportfoliocreators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestigePortfolioCreatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
