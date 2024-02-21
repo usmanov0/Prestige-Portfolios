675d3612a50a3944e045e03d94e413347604a0d4
usmanov <azikusmanov882@gmail.com>
2024-02-21 11:32:18 +0500

    findAllWithPagination,findAllLikeWithPagination

---
+++ src/main/java/com/example/prestigeportfoliocreators/repository/BlogRepository.java
@@ -0,0 +1,22 @@
+package com.example.prestigeportfoliocreators.repository;
+
+import com.example.prestigeportfoliocreators.models.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long>{
+    @Query(value = "SELECT * FROM blogs b "
+            + "WHERE UPPER(CONCAT(b.title, ' ', b.description, ' ', b.body)) LIKE CONCAT('%',UPPER(:search),'%')", nativeQuery = true)
+    public Page<Blog> findAllWithPagination(Pageable pageable, @Param("search") String search);
+
+    @Query(value = "SELECT b.id, b.body, b.date, b.like_count, b.title, b.description, b.unix_time, b.view_count FROM blogs b "
+            + "LEFT JOIN (blog_user b_u JOIN users u on u.id=b_u.user_id) ON b.id=b_u.blog_id "
+            + "WHERE u.address=:address AND UPPER(CONCAT(b.title, ' ', b.description, ' ', b.body)) LIKE CONCAT('%',UPPER(:search),'%')", nativeQuery = true)
+    public Page<Blog> findAllLikedWithPagination(Pageable pageable, @Param("address") String address, @Param("search") String search);
+
+}
