890cff88a05f3d7dba61f3920f1aaeab31b8174a
usmanov <azikusmanov882@gmail.com>
2024-03-06 12:31:39 +0500

    main.

--- src/main/java/com/example/prestigeportfoliocreators/PrestigePortfolioCreatorsApplication.java
+++ src/main/java/com/example/prestigeportfoliocreators/PrestigePortfolioCreatorsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrestigePortfolioCreatorsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PrestigePortfolioCreatorsApplication.class, args);
     }
